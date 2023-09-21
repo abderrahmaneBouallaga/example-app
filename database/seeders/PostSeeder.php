520d66e16da84830e1d05ab08b2db563c87e8bd4
Abderrahmane Bouallaga <106495994+abderrahmaneBouallaga@users.noreply.github.com>
2023-09-21 16:51:53 +0100

    make a relationships

--- database/seeders/PostSeeder.php
+++ database/seeders/PostSeeder.php
@@ -2,16 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Post;
 use Illuminate\Database\Seeder;
+use Database\Seeders\Traits\TruncateTable;
+use Database\Seeders\Traits\DisableForeignKeys;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 class PostSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
+    use TruncateTable, DisableForeignKeys;
+
     public function run(): void
     {
-        //
+        $this->disableForeignKeys();
+        $this->truncate('posts');
+        Post::factory(3)->create();
+        $this->enableForeignKeys();
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,21 +2,24 @@
 
 namespace Database\Seeders;
 
-use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 use Database\Seeders\Traits\TruncateTable;
 use Database\Seeders\Traits\DisableForeignKeys;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
-class CommentSeeder extends Seeder
+class UserSeeder extends Seeder
 {
     use TruncateTable, DisableForeignKeys;
-
+    /**
+     * Run the database seeds.
+     */
     public function run(): void
     {
         $this->disableForeignKeys();
-        $this->truncate('comments');
-        Comment::factory(3)->create();
+        $this->truncate('users');
+        $users = User::factory(10)->create();
         $this->enableForeignKeys();
     }
 }
